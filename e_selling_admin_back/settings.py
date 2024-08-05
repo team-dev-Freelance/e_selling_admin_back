69679e9f1dc8799769a208dedb88a1370421d5d4
koire-dev <wawouam@gmail.com>
2024-08-05 08:35:50 +0100

    commit v10

--- e_selling_admin_back/settings.py
+++ e_selling_admin_back/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-^k#(#tk5!gslwc=aj6x=vubs5n%m-+)u(+$gn*texj50p8aiv8
 DEBUG = True
 
 ALLOWED_HOSTS = ['http://localhost:4200', '*']
-CORS_ALLOWED_ORIGINS = ['http://localhost:4200', '*']
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 # CSRF_TRUSTED_ORIGINS = ['https://esellingadminback-production.up.railway.app']
 
 APPEND_SLASH = False
@@ -59,7 +59,18 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +79,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'e_selling_admin_back.urls'
 
