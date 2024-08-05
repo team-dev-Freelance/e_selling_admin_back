6c15b3db848a173a146ac77173b3dffdf3375791
koire-dev <wawouam@gmail.com>
2024-08-05 07:48:57 +0100

    commit v09

--- e_selling_admin_back/settings.py
+++ e_selling_admin_back/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-^k#(#tk5!gslwc=aj6x=vubs5n%m-+)u(+$gn*texj50p8aiv8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://localhost:4200', '*']
 # ALLOWED_HOSTS = ['esellingadminback-production.up.railway.app']
 # CSRF_TRUSTED_ORIGINS = ['https://esellingadminback-production.up.railway.app']
 
