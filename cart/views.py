343ad63530e4f683b3b4734373b6985a6d0a3ab0
koire-dev <wawouam@gmail.com>
2024-08-22 21:23:11 +0100

    commit v20

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,5 @@
 # views.py
 
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,7 +8,7 @@ from .models import Cart, CartItem
 from .serializers import CartSerializer
 
 
-class CartViewSet(APIView):
+class CartView(APIView):
     def get(self, request):
         cart, created = Cart.objects.get_or_create(user=request.user)
         serializer = CartSerializer(cart)
