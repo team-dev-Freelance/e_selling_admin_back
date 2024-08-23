76d6955834390d79c58fa77745ad262639bff730
koire-dev <wawouam@gmail.com>
2024-08-23 02:13:37 +0100

    commit v20

--- cart/views.py
+++ cart/views.py
@@ -32,24 +32,24 @@ class CartView(APIView):
             return Response({'error': 'Vous devez être connecté pour ajouter des articles au panier.'},
                             status=status.HTTP_401_UNAUTHORIZED)
 
-        article_id = request.data.get('article_id')
+        client_id = request.data.get('clientId')
+        article_id = request.data.get('articleId')
         quantity = request.data.get('quantity', 1)
-        client_id = request.data.get('client_id')
-        if not client_id:
-            return Response({'error': 'Client ID est requis.'}, status=status.HTTP_400_BAD_REQUEST)
-        if request.user.client.id != client_id:
-            return Response({'error': 'Client ID non autorisé.'}, status=status.HTTP_403_FORBIDDEN)
 
-        # client = getattr(request.user, 'client', None)
-        # if not client:
-        #     return Response({'error': 'Client non trouvé pour cet utilisateur.'}, status=status.HTTP_404_NOT_FOUND)
+        if not client_id or not article_id or quantity is None:
+            return Response({'error': 'Données manquantes.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Assurez-vous que les types de données sont corrects
+        try:
+            client = Client.objects.get(id=client_id)
+        except Client.DoesNotExist:
+            return Response({'error': 'Client non trouvé.'}, status=status.HTTP_404_NOT_FOUND)
 
         try:
             article = Article.objects.get(id=article_id)
         except Article.DoesNotExist:
-            return Response({'error': 'L\'article est inexistent'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'Article non trouvé.'}, status=status.HTTP_404_NOT_FOUND)
 
-        client = Client.objects.get(id=client_id)
         cart, created = Cart.objects.get_or_create(client=client)
         cart_item, item_created = CartItem.objects.get_or_create(cart=cart, article=article)
 
